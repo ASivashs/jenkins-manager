183ebe46e33d97530443cba14b622847bf72c000
Anton Sivashka <anton.sivashko@gmail.com>
2024-10-27 20:38:19 +0300

    Added RHL support
    (cherry picked from commit 744aac4ab1f69ae3503d5d7f8a05fcaf789d74c0)

--- jenkins_manager/utils/settings.py
+++ jenkins_manager/utils/settings.py
@@ -1,14 +1,23 @@
 DEB_DISTRO = (
-        "ubuntu", "debian",
+        "ubuntu", "debian", "linuxmint", "raspbian",
     )
-RPM_DISTRO = (
-        "CentOS", "Red Hat", "Arch Linux", "OpenSUSE", 
-        "Amazon Linux", "Azure Linux",
+RHL_DISTRO = (
+        "rhel", "centos", "fedora", "amzn", "oracle"
     )
+SUSE_DISTRO = (
+        "sles", "opensuse", "suse",
+)
+ARCH_DISTRO = (
+        "arch", "altlinux",
+)
 
-LTS_JENKINS_KEY = "https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key"
-LTS_JENKINS_BINARY = "https://pkg.jenkins.io/debian-stable binary/"
-JENKINS_KEYRINGS_PATH = "/usr/share/keyrings/jenkins-keyring.asc"
+LTS_JENKINS_DEB_KEY = "https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key"
+LTS_JENKINS_DEB_BINARY = "https://pkg.jenkins.io/debian-stable binary/"
+LTS_JENKINS_DEB_KEYRINGS_PATH = "/usr/share/keyrings/jenkins-keyring.asc"
 JENKINS_GPG = "/etc/apt/sources.list.d/jenkins.list"
+
+LTS_JENKINS_RPM_KEY = "https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key"
+LTS_JENKINS_RPM_REPO = "https://pkg.jenkins.io/redhat-stable/jenkins.repo"
+LTS_JENKINS_RPM_KEYRINGS_PATH = "/etc/yum.repos.d/jenkins.repo"
 
 diriterdir = ("admin", "user", "guest")
